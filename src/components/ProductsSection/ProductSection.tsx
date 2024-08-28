332be5ce3a58fbd3826fbe2bbd719574ecd6e09f
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-08-28 20:02:57 +0600

    [fet](ui):make a reusable  ui container

---
+++ src/components/ProductsSection/ProductSection.tsx
@@ -0,0 +1,14 @@
+import Container from "../ui/Container";
+
+
+const ProductSection = () => {
+    return (
+        <Container>
+            <h1>
+                <span className="text-secondary">Products</span>
+            </h1>
+        </Container>
+    );
+};
+
+export default ProductSection;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "../../components/HeroSection/HeroSection";
+import ProductSection from "../../components/ProductsSection/ProductSection";
 
 
 const Home = () => {
     return (
         <div className="min-h-screen ">
            <HeroSection></HeroSection>
+           <ProductSection></ProductSection>
         </div>
     );
 };
